34813100cb50415d258088d4897a1d4321732489
Enriek Naldo Patera <Pateraenriek@gmail.com>
2024-07-31 21:14:01 +0700

    feat: implement edit & delete prompt

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import PromptCard from './PromptCard';
 
-const Profile = () => {
-    return (
-        <div>
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name}</span> Profile
+      </h1>
+      <p className="desc text-left">{desc}</p>
+      <div className="mt-10 promt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Profile
+export default Profile;
